9ddd76bfaf3469a81cb98efb248861926488d3b9
Adam Barnhard <adam.barnhard@digitalreasoning.com>
2015-08-05 15:44:25 -0500

    adding template string html

--- knockout-js-infinite-scroll.js
+++ knockout-js-infinite-scroll.js
@@ -16,6 +16,27 @@
         factory(ko, {});
     }
 }(function (ko, exports) {
+    var loadingHtmlTemplate = '<div class="infinite-scroll-loader" style="margin: 0 auto 1em;height:20px;width:20px;text-align:center;padding:1em;display:inline-block;vertical-align:top;" title="loading">' +
+                                '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"' +
+                                'width="24px" height="30px" viewBox="0 0 24 30" style="enable-background:new 0 0 50 50;" xml:space="preserve">' +
+                                    '<rect x="0" y="10" width="4" height="10" fill="#333" opacity="0.2">' +
+                                      '<animate attributeName="opacity" attributeType="XML" values="0.2; 1; .2" begin="0s" dur="0.6s" repeatCount="indefinite" />' +
+                                      '<animate attributeName="height" attributeType="XML" values="10; 20; 10" begin="0s" dur="0.6s" repeatCount="indefinite" />' +
+                                      '<animate attributeName="y" attributeType="XML" values="10; 5; 10" begin="0s" dur="0.6s" repeatCount="indefinite" />' +
+                                    '</rect>' +
+                                    '<rect x="8" y="10" width="4" height="10" fill="#333"  opacity="0.2">' +
+                                      '<animate attributeName="opacity" attributeType="XML" values="0.2; 1; .2" begin="0.15s" dur="0.6s" repeatCount="indefinite" />' +
+                                      '<animate attributeName="height" attributeType="XML" values="10; 20; 10" begin="0.15s" dur="0.6s" repeatCount="indefinite" />' +
+                                      '<animate attributeName="y" attributeType="XML" values="10; 5; 10" begin="0.15s" dur="0.6s" repeatCount="indefinite" />' +
+                                    '</rect>' +
+                                    '<rect x="16" y="10" width="4" height="10" fill="#333"  opacity="0.2">' +
+                                      '<animate attributeName="opacity" attributeType="XML" values="0.2; 1; .2" begin="0.3s" dur="0.6s" repeatCount="indefinite" />' +
+                                      '<animate attributeName="height" attributeType="XML" values="10; 20; 10" begin="0.3s" dur="0.6s" repeatCount="indefinite" />' +
+                                      '<animate attributeName="y" attributeType="XML" values="10; 5; 10" begin="0.3s" dur="0.6s" repeatCount="indefinite" />' +
+                                    '</rect>' +
+                                '</svg>' +
+                            '</div>';
+
     ko.extenders.infinitescroll = function(target, args) {
         var props            = {},
             Y_AXIS           = 'Y',
@@ -35,7 +56,6 @@
 
         props.numPagesPadding = ko.observable(parseFloat(args.numPagesPadding) || 1);
         props.scrollAxis      = ko.observable(args.scrollAxis ? validateAxis(args.scrollAxis.toString().toUpperCase()) : Y_AXIS);
-
         // viewport dimensions
         props.viewportWidth   = ko.observable(parseInt(args.viewportWidth)  || -1);
         props.viewportHeight  = ko.observable(parseInt(args.viewportHeight) || -1);
